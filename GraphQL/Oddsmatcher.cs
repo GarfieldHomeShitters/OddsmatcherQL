b70fefc5b695cb9126c77e8a71d63f417634e62a
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2024-07-06 18:43:00 +0100

    Add QL client

--- GraphQL/Oddsmatcher.cs
+++ GraphQL/Oddsmatcher.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GraphQL.Client.Abstractions;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+
 
 namespace GraphQL
 {
     public partial class Oddsmatcher : Form
     {
+        private readonly IGraphQLClient GraphQlClient;
+
         public Oddsmatcher()
         {
+            GraphQlClient = new GraphQLHttpClient("https://api.oddsplatform.profitaccumulator.com/graphql", new NewtonsoftJsonSerializer());
             InitializeComponent();
         }
+
     }
 }
