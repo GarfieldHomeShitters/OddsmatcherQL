3b388f5e1b8563db39a3f6424539222f30d9a42a
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2024-07-09 22:54:06 +0100

    Create form, for extra place matcher - init commit

---
+++ GraphQL/ExtraPlace.cs
@@ -0,0 +1,14 @@
+using System.Windows.Forms;
+
+namespace GraphQL
+{
+    public partial class ExtraPlace : Form
+    {
+        
+        
+        public ExtraPlace()
+        {
+            InitializeComponent();
+        }
+    }
+}
