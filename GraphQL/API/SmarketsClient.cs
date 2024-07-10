b085df8f92fb8b0fec77d09acf05fac574fc0d91
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2024-07-10 21:24:22 +0100

    Add new endpoints

--- GraphQL/API/SmarketsClient.cs
+++ GraphQL/API/SmarketsClient.cs
@@ -6,23 +6,25 @@ using System.Threading.Tasks;
 using System.Web;
 using GraphQL.Datatypes;
 using RestSharp;
+using Newtonsoft.Json;
+using RestSharp.Serializers.NewtonsoftJson;
 
 namespace GraphQL.API
 {
     public class SmarketsClient
     {
-        private RestClient _Client = new RestClient("https://api.smarkets.com/v3/");
+        private RestClient _Client = new RestClient("https://api.smarkets.com/v3/", configureSerialization: s => s.UseNewtonsoftJson());
 
         public SmarketsClient()
         {
-            
+
         }
         
         /// <summary>
         /// Gets all horse racing events and returns event name and ID
         /// </summary>
         /// <returns>List<list type="(string, int)"></list></returns>
-        private async Task<List<SmarketEvent>> GetAllHorseRaces()
+        public async Task<List<SmarketEvent>> GetAllHorseRaces()
         {
             DateTime startDateTimeMin = DateTime.UtcNow.Date;
             string formattedDateTime = startDateTimeMin.ToString("yyyy-MM-ddTHH:mm:ssZ");
@@ -33,7 +35,7 @@ namespace GraphQL.API
             return response.Events;
         }
 
-        private async Task<List<SmarketMarket>> GetPlaceMarkets(List<SmarketEvent> smarketEvents)
+        public async Task<List<SmarketMarket>> GetPlaceMarkets(List<SmarketEvent> smarketEvents)
         {
             List<int> eventIdToFetch = new List<int>();
             foreach (SmarketEvent @event in smarketEvents)
@@ -44,17 +46,31 @@ namespace GraphQL.API
             string endpoint = $"events/{string.Join(",", eventIdToFetch)}/markets/?sort=event_id%2Cdisplay_order&popular=false&include_hidden=false";
             RestRequest marketRequest = new RestRequest(endpoint);
             MarketsResponse response = await _Client.GetAsync<MarketsResponse>(marketRequest);
-            return new List<SmarketMarket>(response.markets.Where(x => x.slug == "to-place-3"));
+            List<SmarketMarket> markets = new List<SmarketMarket>(response.markets.Where(x => x.slug.Contains("to-place-")));
+            return markets;
         }
 
-        public List<(string, int)> GetAllHorseRacePlaceMarkets()
+        public async Task<List<SmarketContract>> GetContracts(List<Race> races)
         {
-            return new List<(string, int)>();
+            List<string> marketIdToFetch = new List<string>();
+            foreach (Race race in races)
+            {
+                marketIdToFetch.Add(race.MarketID);
+            }
+
+            string endpoint = $"markets/{string.Join(",", marketIdToFetch)}/contracts/?include_hidden=false";
+            RestRequest contractRequest = new RestRequest(endpoint);
+            ContractResponse response = await _Client.GetAsync<ContractResponse>(endpoint);
+            List<SmarketContract> contracts = new List<SmarketContract>(response.contracts);
+            return contracts;
+        }
+
+        public async Task<Dictionary<string,QuoteResponse>> GetQuotes(Race race)
+        {
+            string endpoint = $"markets/{race.MarketID}/quotes/";
+            RestRequest quoteRequest = new RestRequest(endpoint);
+            return await _Client.GetAsync<Dictionary<string, QuoteResponse>>(endpoint);
         }
         
-        public List<(int, int)> getContracts(SmarketMarket market)
-        {
-            return new List<(int, int)>();
-        }
     }
 }
