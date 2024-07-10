19da60628f15e5f8c4866c0144901f772539e96c
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2024-07-10 21:23:29 +0100

    Update designer

--- GraphQL/ExtraPlace.Designer.cs
+++ GraphQL/ExtraPlace.Designer.cs
@@ -31,11 +31,129 @@ namespace GraphQL
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.epRaceCombo = new Syncfusion.WinForms.ListView.SfComboBox();
+            this.loadRacesButton = new System.Windows.Forms.Button();
+            this.epDatagrid = new Syncfusion.WinForms.DataGrid.SfDataGrid();
+            this.loadDataBtn = new System.Windows.Forms.Button();
+            this.stakeNumeric = new Syncfusion.Windows.Forms.Tools.NumericUpDownExt();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bookmakerCombo = new Syncfusion.WinForms.ListView.SfComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.epRaceCombo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epDatagrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stakeNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookmakerCombo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // epRaceCombo
+            // 
+            this.epRaceCombo.DropDownPosition = Syncfusion.WinForms.Core.Enums.PopupRelativeAlignment.Center;
+            this.epRaceCombo.Location = new System.Drawing.Point(12, 12);
+            this.epRaceCombo.MaxDropDownItems = 12;
+            this.epRaceCombo.Name = "epRaceCombo";
+            this.epRaceCombo.Size = new System.Drawing.Size(243, 23);
+            this.epRaceCombo.Style.TokenStyle.CloseButtonBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.epRaceCombo.TabIndex = 0;
+            this.epRaceCombo.TabStop = false;
+            // 
+            // loadRacesButton
+            // 
+            this.loadRacesButton.Location = new System.Drawing.Point(261, 12);
+            this.loadRacesButton.Name = "loadRacesButton";
+            this.loadRacesButton.Size = new System.Drawing.Size(105, 23);
+            this.loadRacesButton.TabIndex = 1;
+            this.loadRacesButton.Text = "Load Races";
+            this.loadRacesButton.UseVisualStyleBackColor = true;
+            this.loadRacesButton.Click += new System.EventHandler(this.loadRacesButton_Click);
+            // 
+            // epDatagrid
+            // 
+            this.epDatagrid.AccessibleName = "Table";
+            this.epDatagrid.Location = new System.Drawing.Point(12, 93);
+            this.epDatagrid.Name = "epDatagrid";
+            this.epDatagrid.Size = new System.Drawing.Size(776, 415);
+            this.epDatagrid.TabIndex = 2;
+            this.epDatagrid.Text = "sfDataGrid1";
+            // 
+            // loadDataBtn
+            // 
+            this.loadDataBtn.Location = new System.Drawing.Point(261, 54);
+            this.loadDataBtn.Name = "loadDataBtn";
+            this.loadDataBtn.Size = new System.Drawing.Size(105, 23);
+            this.loadDataBtn.TabIndex = 3;
+            this.loadDataBtn.Text = "Load Race Data";
+            this.loadDataBtn.UseVisualStyleBackColor = true;
+            this.loadDataBtn.Click += new System.EventHandler(this.loadDataBtn_Click);
+            // 
+            // stakeNumeric
+            // 
+            this.stakeNumeric.BeforeTouchSize = new System.Drawing.Size(243, 20);
+            this.stakeNumeric.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
+            this.stakeNumeric.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.stakeNumeric.DecimalPlaces = 2;
+            this.stakeNumeric.Increment = new decimal(new int[] { 25, 0, 0, 131072 });
+            this.stakeNumeric.Location = new System.Drawing.Point(12, 57);
+            this.stakeNumeric.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            this.stakeNumeric.Name = "stakeNumeric";
+            this.stakeNumeric.Size = new System.Drawing.Size(243, 20);
+            this.stakeNumeric.TabIndex = 4;
+            this.stakeNumeric.ThemeName = "Metro";
+            this.stakeNumeric.Value = new decimal(new int[] { 125, 0, 0, 131072 });
+            this.stakeNumeric.VisualStyle = Syncfusion.Windows.Forms.VisualStyle.Metro;
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Stake:";
+            // 
+            // bookmakerCombo
+            // 
+            this.bookmakerCombo.DropDownPosition = Syncfusion.WinForms.Core.Enums.PopupRelativeAlignment.Center;
+            this.bookmakerCombo.Location = new System.Drawing.Point(388, 12);
+            this.bookmakerCombo.Name = "bookmakerCombo";
+            this.bookmakerCombo.Size = new System.Drawing.Size(400, 23);
+            this.bookmakerCombo.Style.TokenStyle.CloseButtonBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.bookmakerCombo.TabIndex = 6;
+            this.bookmakerCombo.TabStop = false;
+            // 
+            // ExtraPlace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(800, 520);
+            this.Controls.Add(this.bookmakerCombo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stakeNumeric);
+            this.Controls.Add(this.loadDataBtn);
+            this.Controls.Add(this.epDatagrid);
+            this.Controls.Add(this.loadRacesButton);
+            this.Controls.Add(this.epRaceCombo);
+            this.Name = "ExtraPlace";
             this.Text = "ExtraPlace";
+            this.Load += new System.EventHandler(this.ExtraPlace_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.epRaceCombo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epDatagrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stakeNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookmakerCombo)).EndInit();
+            this.ResumeLayout(false);
         }
+
+        private Syncfusion.WinForms.ListView.SfComboBox bookmakerCombo;
+
+
+        private System.Windows.Forms.Label label1;
+
+        private Syncfusion.Windows.Forms.Tools.NumericUpDownExt stakeNumeric;
+
+        private System.Windows.Forms.Button loadRacesButton;
+        private Syncfusion.WinForms.DataGrid.SfDataGrid epDatagrid;
+        private System.Windows.Forms.Button loadDataBtn;
+
+        private Syncfusion.WinForms.ListView.SfComboBox epRaceCombo;
 
         #endregion
     }
