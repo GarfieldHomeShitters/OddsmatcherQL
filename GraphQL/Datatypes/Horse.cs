a2ea12736c155f6c77d31793f7e8b15aa36a9b14
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2024-07-10 03:26:18 +0100

    Begin outlining Horse info

--- GraphQL/Datatypes/Horse.cs
+++ GraphQL/Datatypes/Horse.cs
@@ -1,13 +1,16 @@
+using System;
+
 namespace GraphQL.Datatypes
 {
     public class Horse
     {
+        public string DisplayName;
         public string Name;
         public decimal backOdds;
         public decimal layOdds;
         public decimal backPlaceOdds;
         public decimal layPlaceOdds;
-
+        public int contractID;
         public Horse(string Name, decimal backOdds, decimal layOdds)
         {
             this.Name = Name;
@@ -16,9 +19,13 @@ namespace GraphQL.Datatypes
             backPlaceOdds = ((backOdds - 1) / 5) + 1;
         }
 
-        public void addLayPlaceOdds(decimal odds)
+        public void addSmarketInfo(string displayName, int percentagePrice)
         {
-            this.layPlaceOdds = odds;
+            DisplayName = displayName;
+            // Looks weird, but we have to cast one to a decimal to get out a decimal from the division
+            // Decimal over float so that we have increased precision, not really needed because we round 
+            // but the standard throughout the API has always been to use a decimal. 
+            layPlaceOdds = Math.Round( ((decimal)100000 / percentagePrice) , 2);
         }
     }
 }
