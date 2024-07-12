a2ab2d2c54b84fa852ddc90816ad088e5e0aa21b
SoulessAdam <58148487+SoulessAdam@users.noreply.github.com>
2024-07-12 20:32:30 +0100

    Add link to race as well as start using id by default and selectionName as fallback (apostrophe case)

--- GraphQL/Datatypes/Horse.cs
+++ GraphQL/Datatypes/Horse.cs
@@ -16,15 +16,15 @@ namespace GraphQL.Datatypes
             this.Name = Name;
             this.backOdds = backOdds;
             this.layOdds = layOdds;
-            backPlaceOdds = ((backOdds - 1) / 5) + 1;
+            backPlaceOdds = ((this.backOdds - 1) / 5) + 1;
         }
 
-        public void addSmarketInfo(int percentagePrice)
+        public void addSmarketInfo(int percentagePrice =88888 )
         {
             // Looks weird, but we have to cast one to a decimal to get out a decimal from the division
             // Decimal over float so that we have increased precision, not really needed because we round 
             // but the standard throughout the API has always been to use a decimal. 
-            layPlaceOdds = Math.Round( ((decimal)100000 / percentagePrice) , 2);
+            layPlaceOdds = Math.Round( ((decimal)10000 / percentagePrice) , 2);
         }
     }
 }
